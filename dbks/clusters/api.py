5f31153ab75dcb7b914d1c86a305ceefd7e5fa29
Vincent Lam <vincent.lam@cloudten.com.au>
2020-10-22 09:57:51 +1100

    add clusters api tests

--- dbks/clusters/api.py
+++ dbks/clusters/api.py
@@ -2,47 +2,47 @@ class ClusterAPI:
     def __init__(self, client):
         self.client = client
 
-    def create(self, data):
+    def create(self, params=None, data=None):
         return self.client.call("POST", "/clusters/create", data=data)
 
-    def edit(self, data):
+    def edit(self, params=None, data=None):
         return self.client.call("POST", "/clusters/edit", data=data)
 
-    def start(self, data):
+    def start(self, params=None, data=None):
         return self.client.call("POST", "/clusters/start", data=data)
 
-    def restart(self, data):
+    def restart(self, params=None, data=None):
         return self.client.call("POST", "/clusters/restart", data=data)
 
-    def resize(self, data):
+    def resize(self, params=None, data=None):
         return self.client.call("POST", "/clusters/resize", data=data)
 
-    def terminate(self, data):
+    def terminate(self, params=None, data=None):
         return self.client.call("POST", "/clusters/delete", data=data)
 
-    def delete(self, data):
+    def delete(self, params=None, data=None):
         return self.client.call("POST", "/clusters/permanent-delete", data=data)
 
-    def get(self, params):
+    def get(self, params=None, data=None):
         return self.client.call("GET", "/clusters/get", params=params)
 
-    def pin(self, data):
+    def pin(self, params=None, data=None):
         return self.client.call("POST", "/clusters/pin", data=data)
 
-    def unpin(self, data):
+    def unpin(self, params=None, data=None):
         return self.client.call("POST", "/clusters/unpin", data=data)
 
-    def list(self):
+    def list(self, params=None, data=None):
         return self.client.call("GET", "/clusters/list")
 
-    def list_node_types(self):
+    def list_node_types(self, params=None, data=None):
         return self.client.call("GET", "/clusters/list-node-types")
 
-    def runtime_versions(self):
+    def runtime_versions(self, params=None, data=None):
         return self.client.call("GET", "/clusters/spark-versions")
 
-    def list_zones(self):
+    def list_zones(self, params=None, data=None):
         return self.client.call("GET", "/clusters/list-zones")
 
-    def events(self, data):
+    def events(self, params=None, data=None):
         return self.client.call("POST", "/clusters/events", data=data)
--- tests/test_clusters_api.py
+++ tests/test_clusters_api.py
@@ -1,18 +1,44 @@
+from _pytest.mark import param
+import pytest
 from dbks.client import Client
 from dbks.clusters.api import ClusterAPI
 from unittest.mock import patch
 
 
-def test_clusters_api_create(monkeypatch):
+@pytest.mark.parametrize(
+    "inputs, expect, result",
+    [
+        (["create", None, {"a": "1"}], ["POST", "create", {}, {"a": "1"}], True),
+        (["edit", None, {"a": "1"}], ["POST", "edit", {}, {"a": "1"}], True),
+        (["start", None, {"a": "1"}], ["POST", "start", {}, {"a": "1"}], True),
+        (["restart", None, {"a": "1"}], ["POST", "restart", {}, {"a": "1"}], True),
+        (["resize", None, {"a": "1"}], ["POST", "resize", {}, {"a": "1"}], True),
+        (["terminate", None, {"a": "1"}], ["POST", "delete", {}, {"a": "1"}], True),
+        (
+            ["delete", None, {"a": "1"}],
+            ["POST", "permanent-delete", {}, {"a": "1"}],
+            True,
+        ),
+        (["get", {"a": "1"}, None], ["GET", "get", {"a": "1"}, {}], True),
+        (["pin", None, {"a": "1"}], ["POST", "pin", {}, {"a": "1"}], True),
+        (["unpin", None, {"a": "1"}], ["POST", "unpin", {}, {"a": "1"}], True),
+        (["list", None, None], ["GET", "list", {}, {}], True),
+        (["list_node_types", None, None], ["GET", "list-node-types", {}, {}], True),
+        (["runtime_versions", None, None], ["GET", "spark-versions", {}, {}], True),
+        (["list_zones", None, None], ["GET", "list-zones", {}, {}], True),
+        (["events", None, {"a": "1"}], ["POST", "events", {}, {"a": "1"}], True),
+    ],
+)
+def test_clusters_api(monkeypatch, inputs, expect, result):
     monkeypatch.setenv("DBC_TOKEN", "fake_token")
     with patch("dbks.client.Session.request") as mock:
         client = Client("databricks.com")
         clusters_api = ClusterAPI(client)
-        clusters_api.create({"a": "1"})
+        getattr(clusters_api, inputs[0])(params=inputs[1], data=inputs[2])
         mock.assert_called_once_with(
-            "POST",
-            "https://databricks.com/api/2.0/clusters/create",
-            params={},
-            data={"a": "1"},
+            expect[0],
+            f"https://databricks.com/api/2.0/clusters/{expect[1]}",
+            params=expect[2],
+            data=expect[3],
             headers={"Authorization": "Bearer fake_token"},
         )
