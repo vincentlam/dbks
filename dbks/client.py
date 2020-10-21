39f393cc17cdce21278e26dc3440810bca2e0b30
Vincent Lam <vincent.lam@cloudten.com.au>
2020-10-21 15:37:56 +1100

    add client

---
+++ dbks/client.py
@@ -0,0 +1,27 @@
+import copy
+import os
+from requests import Session
+
+
+class Client:
+    def __init__(self, host: str, version: str = "2.0"):
+        self.host = host
+        self.version = version
+        self.token = os.getenv(f"DBC_TOKEN", None)
+        self.headers = {"Authorization": f"Bearer {self.token}"}
+        self.session = Session()
+
+    @property
+    def url(self):
+        return f"https://{self.host}/api/{self.version}"
+
+    def call(self, method, endpoint, params={}, data={}, headers=None):
+        if headers is None:
+            headers = self.headers
+        else:
+            tmp_headers = copy.deepcopy(self.headers)
+            tmp_headers.update(headers)
+            headers = tmp_headers
+        return self.session.request(
+            method, self.url + endpoint, params=params, data=data, headers=headers
+        )
---
+++ tests/test_client.py
@@ -0,0 +1,17 @@
+from requests.api import head
+from dbks.client import Client
+from unittest.mock import patch
+
+
+def test_client_call(monkeypatch):
+    monkeypatch.setenv("DBC_TOKEN", "fake_token")
+    with patch("dbks.client.Session.request") as mock:
+        client = Client("databricks.com")
+        client.call("GET", "/fake", {"a": "1"})
+        mock.assert_called_once_with(
+            "GET",
+            "https://databricks.com/api/2.0/fake",
+            params={"a": "1"},
+            data={},
+            headers={"Authorization": "Bearer fake_token"},
+        )
