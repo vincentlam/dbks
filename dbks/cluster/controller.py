33fb27d5383eb5934d642fc9fcc56e7c0330b84a
Vincent Lam <vincentlam@live.com.au>
2020-10-23 03:23:27 +1030

    add ResponseHandler; add ClusterController

--- dbks/cluster/controller.py
+++ dbks/cluster/controller.py
@@ -1,9 +1,59 @@
+from dbks.response_handler import ResponseHandler
+
+
 class ClusterController:
     def __init__(self, api):
         self.api = api
 
-    def a(self, x):
-        print("a")
+    @ResponseHandler
+    def list(self):
+        return self.api.list()
 
-    def b(self, y):
-        print("b")
+    @ResponseHandler
+    def get(self, cluster_id):
+        return self.api.get(params={"cluster_id": cluster_id})
+
+    @ResponseHandler
+    def create(self, cluster_def):
+        return self.api.create(json=cluster_def)
+
+    @ResponseHandler
+    def edit(self, cluster_id, cluster_def):
+        cluster_def["cluster_id"] = cluster_id
+        return self.api.edit(json=cluster_def)
+
+    @ResponseHandler
+    def start(self, cluster_id):
+        return self.api.start(json={"cluster_id": cluster_id})
+
+    @ResponseHandler
+    def pin(self, cluster_id):
+        return self.api.pin(json={"cluster_id": cluster_id})
+
+    @ResponseHandler
+    def unpin(self, cluster_id):
+        return self.api.unpin(json={"cluster_id": cluster_id})
+
+    def get_id_by_name(self, cluster_name=""):
+        cluster_ids = []
+        if not cluster_name:
+            raise ValueError("Please provide cluster name string!")
+        res = self.list()
+        clusters = res.json()["clusters"]
+        for cluster in clusters:
+            if cluster["cluster_name"] == cluster_name:
+                cluster_ids.append(cluster["cluster_id"])
+        return cluster_ids
+
+    def create_or_edit_cluster(self, cluster_def):
+        cluster_ids = self.get_id_by_name(cluster_def["cluster_name"])
+        if not cluster_ids:
+            self.create(cluster_def)
+        elif len(cluster_ids) == 1:
+            pass
+            # compare
+            # self.edit(cluster_ids[0], cluster_def)
+        else:
+            raise Exception(
+                f'Found multiple clusters {cluster_ids} with name [{cluster_def["cluster_name"]}]!'
+            )
--- dbks/response_handler.py
+++ dbks/response_handler.py
@@ -1,11 +1,26 @@
-class ResponseHandler(object):
-    def __init__(self):
-        self
+from functools import partial
+from requests import Response
 
-    def __getattribute__(self, attr):
-        method = object.__getattribute__(self, attr)
-        if not method:
-            raise Exception("Method %s not implemented" % attr)
-        if callable(method):
-            print("I am:")
-        return method
+
+class ResponseHandler(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, instance, *args, **kwargs):
+        print(f'"{instance.__class__.__name__}.{self.func.__name__}" triggered with')
+        if args:
+            print("args:", args)
+        if kwargs:
+            print("kwargs:", kwargs)
+        res = self.func(instance, *args, **kwargs)
+        if type(res) == Response:
+            print("Response:")
+            print("status_code:", str(res.status_code))
+            # print('text:', res.text)
+            print("json:", res.json())
+            if str(res.status_code)[0] != "2":
+                raise SystemError("Non 2XX status code returned!")
+        return res
+
+    def __get__(self, instance, owner):
+        return partial(self, instance)
